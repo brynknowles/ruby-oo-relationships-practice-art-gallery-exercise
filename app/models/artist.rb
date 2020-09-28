7d4f2228d8c2d5d0aca5460c22de5a3df63e8f23
brynknowles <bryn.knowles@gmail.com>
2020-09-28 19:52:21 -0400

    practice test completed

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,48 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+  def paintings
+    Painting.all.select { |painting| painting.artist == self}
+  end
+
+  def galleries
+    self.paintings.map { |painting| painting.gallery}.uniq
+  end
+
+  def cities
+    self.galleries.map { |gallery| gallery.city}.uniq
+  end
+
+  def self.total_experience
+    self.all.sum { |artist| artist.years_experience}
+  end
+
+  def number_of_paintings
+    self.paintings.count
+  end
+
+  def paintings_per_year
+    self.number_of_paintings.to_f / self.years_experience.to_f
+  end
+
+  def self.most_prolific
+    self.all.max_by { |artist| artist.paintings_per_year}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,17 @@
 require_relative '../config/environment.rb'
 
+ari = Artist.new("Ari", 2)
+ziggy = Artist.new("Ziggy", 6)
+
+met = Gallery.new("The Met", "NYC")
+moma = Gallery.new("MOMA", "NYC")
+louvre = Gallery.new("The Louvre Museum", "Paris")
+
+painting1 = Painting.new("A House", 400, ari, met)
+painting2 = Painting.new("My Bed", 1000, ziggy, louvre)
+painting3 = Painting.new("A Cactus", 1200, ari, moma)
+painting4 = Painting.new("A Nap", 500, ziggy, louvre)
+painting5 = Painting.new("A Volcano", 10000, ari, louvre)
 
 
 
